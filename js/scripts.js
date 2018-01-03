66cc30b17e925d2bdc34634348e255d9c3d83301
xenogonx <pablez@wp.pl>
2018-01-03 11:07:52 +0100

    css-changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,16 @@ var allNames = [];
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
-//połączenie tablic za pomocą concat
-var allNames = allNames.concat(femaleNames,maleNames);
+// połączenie tablic za pomocą concat
+var allNames = allNames.concat(femaleNames, maleNames);
 var newName = prompt('wpisz imię: ');
 
-//sprawdzamy czy nie ma takiego imienia w tablicy
+// sprawdzamy czy nie ma takiego imienia w tablicy
 if ((allNames.indexOf(newName)) === -1){
   allNames.push(newName);
   alert(allNames);
   console.log(allNames)
 } else { 
-
+  
   alert('imię jest już na liście');
 }
-
-
-
